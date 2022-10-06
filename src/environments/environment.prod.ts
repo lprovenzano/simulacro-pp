7e1fda83b4eaac84225adf1c9e4276f3f636a129
Luca Eros Provenzano <luca.provenzano@mercadolibre.com>
2022-10-06 09:59:24 -0300

    init

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'simulacro-pp-7f6ec',
+    appId: '1:682574981820:web:a9bd5dab59cff5e156feb9',
+    storageBucket: 'simulacro-pp-7f6ec.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyB9Fgvp61UH-_EaTVN5uJLZnLRgzVgWqGg',
+    authDomain: 'simulacro-pp-7f6ec.firebaseapp.com',
+    messagingSenderId: '682574981820',
+    measurementId: 'G-ZQP0JK0C34',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'simulacro-pp-7f6ec',
+    appId: '1:682574981820:web:a9bd5dab59cff5e156feb9',
+    storageBucket: 'simulacro-pp-7f6ec.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyB9Fgvp61UH-_EaTVN5uJLZnLRgzVgWqGg',
+    authDomain: 'simulacro-pp-7f6ec.firebaseapp.com',
+    messagingSenderId: '682574981820',
+    measurementId: 'G-ZQP0JK0C34',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
